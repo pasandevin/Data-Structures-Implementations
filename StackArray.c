25616c5bf23800b3161e150505ff01256fe449cf
pasandevin <jayawardene.pasandevin@gmail.com>
2021-10-20 21:47:03 +0530

    stack array: functions pop,push

--- StackArray.c
+++ StackArray.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAXSTACKSIZE 20
 
-typedef int stackEntryType;
+typedef char stackEntryType;
 typedef struct Stack
 {
     int top;
+    int no_of_elements;
     stackEntryType data[MAXSTACKSIZE];
 }Stack;
 
@@ -21,13 +23,53 @@ int isStackFull(Stack *S) {
     return S->top == MAXSTACKSIZE - 1;
 }
 
+void push(Stack *S, stackEntryType item) {
+    if (isStackFull(S)) {
+        printf("Stack is Full");
+        exit(1);
+    }
+    S->data[++S->top] = item;
+    S->no_of_elements++;
+}
+
+void pop(Stack *S, stackEntryType *item) {
+    if (isStackEmpty(S)) {
+        printf("Stack is Empty");
+        exit(1);
+    }
+    *item = S->data[S->top--];
+    S->no_of_elements--;
+}
 
 int main() {
     Stack S;
+    char c;
     
     createStack(&S);
-    S.data[3] = 3;
-    printf("%d",isStackEmpty(&S));
+
+    char msg[] = "Hello World";
+
+    printf("The Array to push to the Stack is :\t%s", msg);
+
+    for (int i = 0; msg[i] != '\0'; i++) {
+        push(&S, msg[i]);
+    }
+    printf("\n\n");
+
+    for (int i = 0; msg[i] != 'k'; i++) {
+        pop(&S, &c);
+        putchar(c);
+    }
+
+    // while (isStackEmpty(&S))
+    // {
+    //     /* code */
+    // }
+    
+
+
+
+
     
 
     return 0;
