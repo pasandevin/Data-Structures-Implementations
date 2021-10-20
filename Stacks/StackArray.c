dcf002378d8b0df688d5eefb35f1ed8f925bc885
pasandevin <jayawardene.pasandevin@gmail.com>
2021-10-20 22:01:50 +0530

    implemented main function

--- Stacks/StackArray.c
+++ Stacks/StackArray.c
@@ -3,13 +3,18 @@
 
 #define MAXSTACKSIZE 20
 
+
+
 typedef char stackEntryType;
+
 typedef struct Stack
 {
     int top;
     int no_of_elements;
     stackEntryType data[MAXSTACKSIZE];
 }Stack;
+
+
 
 void createStack(Stack *S) {
     S->top = -1;
@@ -41,6 +46,8 @@ void pop(Stack *S, stackEntryType *item) {
     S->no_of_elements--;
 }
 
+
+
 int main() {
     Stack S;
     char c;
@@ -49,28 +56,21 @@ int main() {
 
     char msg[] = "Hello World";
 
-    printf("The Array to push to the Stack is :\t%s", msg);
+    printf("The String to push\t: %s", msg);
 
     for (int i = 0; msg[i] != '\0'; i++) {
         push(&S, msg[i]);
     }
-    printf("\n\n");
 
-    for (int i = 0; msg[i] != 'k'; i++) {
+    printf("\n\n");
+    printf("The String from Stack\t: ");
+
+    while (!isStackEmpty(&S))
+    {
         pop(&S, &c);
         putchar(c);
     }
 
-    // while (isStackEmpty(&S))
-    // {
-    //     /* code */
-    // }
     
-
-
-
-
-    
-
     return 0;
 }
